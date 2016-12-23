2b85ea3791ca39b747ab9abb350dc094d83a6491
ChazalFlorian <chazal.florian@gmail.com>
2016-12-23 17:46:04 +0100

    [Routes] home | pokemon | detail

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,23 +2,35 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { ListComponent } from './list/list.component';
 import { DetailComponent } from './detail/detail.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+
+const appRoutes: Routes = [
+  { path: '/', component: HomeComponent },
+  { path: '/pokemon', component: ListComponent },
+  { path: '/pokemon/:id', component: DetailComponent },
+
+  { path: '**', component: NotFoundComponent }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     ListComponent,
-    DetailComponent
+    DetailComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+      RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/detail/detail.component.ts
+++ src/app/detail/detail.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-detail',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DetailComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+      private  route: ActivatedRoute
+  ) { }
 
   ngOnInit() {
+    let id = this.route.snapshot.params['id'];
+    //add here a fetch pokemon data function
   }
 
 }
