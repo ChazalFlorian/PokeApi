5426e1e4f19a16910bc4b9461da3d726cb2b9250
ChazalFlorian <chazal.florian@gmail.com>
2016-12-23 13:30:36 +0100

    [Init] angular2-cli w/ ng2-material | blank pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,16 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ListComponent } from './list/list.component';
+import { DetailComponent } from './detail/detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ListComponent,
+    DetailComponent
   ],
   imports: [
     BrowserModule,
